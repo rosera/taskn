8fe30865243957f3b268215138a411f59edec84d
rosera <askrichardrose@gmail.com>
2023-07-10 10:40:42 +0100

    Add: sequence processing

--- ownr/ownr.go
+++ ownr/ownr.go
@@ -1,34 +1,39 @@
 package main
 
-
 import (
 	"fmt"
   "os"
-  "strings"
 )
 
 const (
   file    = "QL_OWNER"
-  content = "# Lab Owner\nlab-architects@google.com # lab-architects@google.com"
+  content = "# Lab Owner\nlab-architects@google.com # lab-architects@google.com\n"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Please provide the filename as a command-line argument")
+		fmt.Println("Please provide the folder path as a command-line argument")
 		return
 	}
 
 	filename := os.Args[1]
 
-	// Check if the filename contains "QL_OWNER"
-	if strings.Contains(filename, file) {
-	  err := deleteFileIfExists(filename)
-	  if err != nil {
-	  	fmt.Println(err)
-      return
-	  } else {
-      // Create a new file
-      writeStringToFile(filename, content) 
-	  }   
-  }
+  var delimit = "/"
+  input := filename + delimit + file
+  
+	// Delete the original file
+	err := deleteFileIfExists(input)
+
+	if err != nil {
+	   fmt.Println(err)
+     return
+	}   
+
+  // Create a new file
+  writeStringToFile(input, content) 
+
+	if err != nil {
+	   fmt.Println(err)
+     return
+	}   
 }
--- ownr/write.go
+++ ownr/write.go
@@ -1,13 +1,11 @@
 package main
 
-
 import (
 	"fmt"
   "io/ioutil"
 )
 
 func writeStringToFile(filename string, content string) error {
-
 	err := ioutil.WriteFile(filename, []byte(content), 0644)
 	if err != nil {
 		return fmt.Errorf("failed to write to file: %v", err)
